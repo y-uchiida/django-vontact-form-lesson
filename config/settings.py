b6f5b867830f8c8c822ffed832da50304b9fe73e
y-uchiida <y.uchiida@nirneege.com>
2024-06-05 11:24:51 +0900

    PostgreSQLを利用するようにDB設定を行う

--- config/settings.py
+++ config/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'contact_form',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
