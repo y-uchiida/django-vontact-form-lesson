fbd13d2b00d7214ac97e5fcbee1f696e68684cec
y-uchiida <y.uchiida@nirneege.com>
2024-06-05 14:14:22 +0900

    contacts アプリケーションを Django プロジェクトに登録

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
