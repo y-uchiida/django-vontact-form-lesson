a772667a37ff293b4e83394f847399151ea3edf1
y-uchiida <y.uchiida@nirneege.com>
2024-06-05 15:23:53 +0900

    お問い合わせフォームのView クラスを作成する

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,76 @@
+from django.views import generic
+from django.urls import reverse_lazy
 from django.shortcuts import render
+from .forms import CorporateContactForm, IndividualContactForm
+from .models import IndividualContact, CorporateContact
+ 
+class Home(generic.TemplateView):
+    template_name = 'contacts/index.html'
+ 
+ 
+class IndividualContactInput(generic.FormView):
+    template_name = 'contacts/individual_contact_input.html'
+    form_class = IndividualContactForm
+ 
+    def form_valid(self, form):
+        return render(self.request, IndividualContactInput.template_name, {'form': form})
+ 
+ 
+class IndividualContactConﬁrm(generic.FormView):
+    form_class = IndividualContactForm
+ 
+    def form_valid(self, form):
+        return render(self.request, 'contacts/individual_contact_conﬁrm.html', {'form': form})
+ 
+    def form_invalid(self, form):
+        return render(self.request, IndividualContactInput.template_name, {'form': form})
+ 
+ 
+class IndividualContactCreate(generic.CreateView):
+    success_url = reverse_lazy('contacts:form_send_complete')
+    form_class = IndividualContactForm
 
-# Create your views here.
+    
+    def form_invalid(self, form):
+        return render(self.request, IndividualContactInput.template_name, {'form': form})
+
+
+    def form_valid(self, form):
+        IndividualContact.email_users(form, 'sample@internetacademy.co.jp')
+        return super().form_valid(form)
+
+
+class CorporateContactInput(generic.FormView):
+    template_name = 'contacts/corporate_contact_input.html'
+    form_class = CorporateContactForm
+
+
+    def form_valid(self, form):
+        return render(self.request, CorporateContactInput.template_name, {'form': form})
+
+
+class CorporateContactConﬁrm(generic.FormView):
+    form_class = CorporateContactForm
+    
+    def form_valid(self, form):
+        return render(self.request, 'contacts/corporate_contact_conﬁrm.html', {'form': form})
+    
+    
+    def form_invalid(self, form):
+        return render(self.request, CorporateContactInput.template_name, {'form': form})
+
+class CorporateContactCreate(generic.CreateView):
+    form_class = CorporateContactForm
+    success_url = reverse_lazy('contacts:form_send_complete')
+
+
+    def form_invalid(self, form):
+        return render(self.request, CorporateContactInput.template_name, {'form': form})
+    
+    
+    def form_valid(self, form):
+        CorporateContact.email_users(form, 'sample@internetacademy.co.jp')
+        return super().form_valid(form)
+
+class FormSendComplete(generic.TemplateView):
+    template_name = 'contacts/form_send_complete.html'
