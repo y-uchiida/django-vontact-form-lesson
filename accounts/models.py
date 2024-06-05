3f3436dc64e24a8da48c077b2ed6981731913cff
y-uchiida <y.uchiida@nirneege.com>
2024-06-05 13:15:38 +0900

    CustomUser モデルを実装

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+ 
+class CustomUser(AbstractUser):
+    is_received_email = models.BooleanField(verbose_name='お問い合わせメールを受け取るか', default=True)
+ 
+    @staticmethod
+    def get_users_emailed():
+        return CustomUser.objects.filter(is_received_email=True)
+ 
+    @staticmethod
+    def email_users(users, subject, message, from_email=None):
+        for user in users:
+            user.email_user(subject, message, from_email)
